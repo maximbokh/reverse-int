3603492a556a8f8c0074469d2b54daaf06079a4e
maximbokh <isupermaximus@gmail.com>
2023-01-10 16:50:45 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    for (i = String(Math.abs(n)).length - 1; i >= 0; i--) {
+        result += String(Math.abs(n))[i]; 
+      }
+      return result;
 }
